405462529469399b49e9f67e5c799547369ae092
Arthur Gomes Alves <desenvolvimentonit@gmail.com>
2025-09-21 22:37:30 -0300

    Crud de usuarios

--- professor_robot/src/professor_robot/routers/usuario_routers.py
+++ professor_robot/src/professor_robot/routers/usuario_routers.py
@@ -1,8 +1,9 @@
 from http import HTTPStatus
+from typing import List
 
 from fastapi import APIRouter
 
-from ..schemas.usuario.usuario_schema import UsuarioCreate, UsuarioResponse
+from ..schemas.usuario.usuario_schema import UsuarioCreate, UsuarioBase, UsuarioUpdate
 
 usuarios_router = APIRouter()
 
@@ -10,7 +11,7 @@ usuarios = [
 
 ]
 
-@usuarios_router.get("/usuarios/", tags=['Usuarios'], status_code=HTTPStatus.OK, response_model=UsuarioResponse)
+@usuarios_router.get("/usuarios/", tags=['Usuarios'], status_code=HTTPStatus.OK, response_model=List[UsuarioBase])
 def read_users():
     return usuarios
     
@@ -19,7 +20,25 @@ def create_user(usuario: UsuarioCreate):
     usuarios.append(usuario)
     return {'message': 'Sucess'}
 
-# @usuarios_router.get("/usuarios/", status_code=HTTPStatus.OK)
+@usuarios_router.patch("/usuarios/{nome}", tags=['Usuarios'], status_code=HTTPStatus.OK)
+def update_user(nome: str, usuario: UsuarioUpdate):
+    print(usuario)
+    for i, usuario_obj in enumerate(usuarios):
+        if usuario_obj.nome == nome:
+            update_data = usuario.dict(exclude_unset=True)
+            usuarios[i] = usuario_obj.copy(update=update_data)
+            return {'message': 'Usuário atualizado com sucesso'}
+    
+    return {'message': 'Usuário não encontrado'}
+
+@usuarios_router.delete("/usuarios/{nome}", tags=['Usuarios'], status_code=HTTPStatus.OK)
+def update_user(nome: str):
+    for i, usuario_obj in enumerate(usuarios):
+        if usuario_obj.nome == nome:
+            usuarios.remove(usuario_obj)
+            return {'message': 'Usuário removido com sucesso'}
+    
+    return {'message': 'Usuário não encontrado'}
 
 # @usuarios_router.get("/usuarios/", status_code=HTTPStatus.OK)
 
